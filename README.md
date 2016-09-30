9e73c5d687cac342b6ea10a044fbda3fea8e4061
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-09-30 18:06:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Thumbs
+*cakephp-thumber* is a CakePHP plugin to create thumbnails.  
+It can create thumbnails (rectangular or square) of local and remote images.
+
+## Installation
+You can install the plugin via composer:
+
+    $ composer require --prefer-dist mirko-pagliai/cakephp-thumber
+
+## Versioning
+For transparency and insight into our release cycle and to maintain backward 
+compatibility, *Thumbs* will be maintained under the 
+[Semantic Versioning guidelines](http://semver.org).
---
+++ tests/TestCase/Core/GlobalFunctionsTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of cakephp-thumber.
+ *
+ * cakephp-thumber is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * cakephp-thumber is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with cakephp-thumber.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
+ */
+namespace Thumber\Test\TestCase;
+
+use Cake\TestSuite\TestCase;
+
+/**
+ * GlobalFunctionsTest class
+ */
+class GlobalFunctionsTest extends TestCase
+{
+    /**
+     * Test for `isUrl()` global function
+     * @return void
+     * @test
+     */
+    public function testIsUrl()
+    {
+        //Http(s)
+        $this->assertTrue(isUrl('https://www.example.com'));
+        $this->assertTrue(isUrl('http://www.example.com'));
+        $this->assertTrue(isUrl('www.example.com'));
+        $this->assertTrue(isUrl('http://example.com'));
+        $this->assertTrue(isUrl('http://example.com/file'));
+        $this->assertTrue(isUrl('http://example.com/file.html'));
+        $this->assertTrue(isUrl('www.example.com/file.html'));
+        $this->assertTrue(isUrl('http://example.com/subdir/file'));
+
+        //Ftp
+        $this->assertTrue(isUrl('ftp://www.example.com'));
+        $this->assertTrue(isUrl('ftp://example.com'));
+        $this->assertTrue(isUrl('ftp://example.com/file.html'));
+
+        //Missing "http" and/or "www"
+        $this->assertFalse(isUrl('example.com'));
+
+        //Files and dirs
+        $this->assertFalse(isUrl('folder'));
+        $this->assertFalse(isUrl(DS . 'folder'));
+        $this->assertFalse(isUrl(DS . 'folder' . DS));
+        $this->assertFalse(isUrl(DS . 'folder' . DS . 'file.txt'));
+    }
+}
