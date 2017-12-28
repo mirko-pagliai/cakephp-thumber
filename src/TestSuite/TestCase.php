a3ce897ae9bef00242bdb8cdc1bbb1ddd61abc01
mirko-pagliai <mirko.pagliai@gmail.com>
2017-12-28 14:19:26 +0100

    fixed code

--- src/TestSuite/TestCase.php
+++ src/TestSuite/TestCase.php
@@ -148,7 +148,7 @@ abstract class TestCase extends CakeTestCase
         $regex = sprintf(
             '/^%s[a-z0-9]{32}_[a-z0-9]{32}\.(%s)/',
             preg_quote($this->getPath() . DS, '/'),
-            implode('|', ['bmp', 'gif', 'jpg', 'ico', 'png', 'psd', 'tiff'])
+            self::getSupportedFormats()
         );
         self::assertRegExp($regex, $path, $message);
     }
