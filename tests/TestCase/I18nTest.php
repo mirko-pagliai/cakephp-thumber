1df0856b266bd1d3cdc9e4a281baf84fceb9f5b4
mirko-pagliai <mirko.pagliai@gmail.com>
2020-01-21 17:05:01 +0100

    fixed locales and added tests

---
+++ tests/TestCase/I18nTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of cakephp-thumber.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-thumber
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Thumber\Test\TestCase;
+
+use Cake\I18n\I18n;
+use MeTools\TestSuite\TestCase;
+
+/**
+ * I18nTest class
+ */
+class I18nTest extends TestCase
+{
+    /**
+     * Tests I18n translations
+     * @test
+     */
+    public function testI18nConstant()
+    {
+        $translator = I18n::getTranslator('thumber', 'it');
+        $this->assertEquals('Cancella tutte le miniature', $translator->translate('Clears all thumbnails'));
+    }
+}
