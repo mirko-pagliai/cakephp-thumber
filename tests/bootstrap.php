f6f18de397271ce2b73852d0e82e2e6b48ae33a0
mirko-pagliai <mirko.pagliai@gmail.com>
2024-05-09 12:44:48 +0200

    fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,24 +21,15 @@ date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
 
 define('ROOT', dirname(__DIR__) . DS);
-define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('CAKE', CORE_PATH . 'src' . DS);
-define('TESTS', ROOT . 'tests' . DS);
-define('APP', ROOT . 'tests' . DS . 'test_app' . DS);
-define('APP_DIR', 'test_app' . DS);
-define('WEBROOT_DIR', 'webroot' . DS);
-define('WWW_ROOT', APP . 'webroot' . DS);
+const CORE_PATH = ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS;
+const TESTS = ROOT . 'tests' . DS;
+const APP = ROOT . 'tests' . DS . 'test_app' . DS;
+const WWW_ROOT = APP . 'webroot' . DS;
 define('TMP', sys_get_temp_dir() . DS . 'cakephp-thumber' . DS);
-define('CONFIG', APP . 'config' . DS);
-define('CACHE', TMP . 'cache' . DS);
-define('LOGS', TMP . 'logs' . DS);
-define('SESSIONS', TMP . 'sessions' . DS);
+const CONFIG = APP . 'config' . DS;
 
-foreach ([TMP, LOGS, SESSIONS, CACHE . 'views', CACHE . 'models'] as $dir) {
-    if (!file_exists($dir)) {
-        mkdir($dir, 0777, true);
-    }
+if (!file_exists(TMP)) {
+    mkdir(TMP, 0777, true);
 }
 
 require dirname(__DIR__) . '/vendor/autoload.php';
@@ -50,7 +41,7 @@ Configure::write('App', [
     'encoding' => 'UTF-8',
     'base' => false,
     'baseUrl' => false,
-    'dir' => APP_DIR,
+    'dir' => 'test_app' . DS,
     'webroot' => 'webroot',
     'wwwRoot' => WWW_ROOT,
     'fullBaseUrl' => 'http://localhost',
